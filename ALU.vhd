f22541b75c0c6b40e44626634116400ac69e94b9
mechanicallamb <60459171+mechanicallamb@users.noreply.github.com>
2021-11-24 01:11:37 -0500

    Implemented ALU Datapath
    Implemented the datapath for the ALU and connected all internal components.

--- ALU.vhd
+++ ALU.vhd
@@ -19,10 +19,7 @@ entity ALU_DATAPATH is
 	   -- [3:1] DA, the register to be written to on a write
 	   -- [0] read or write to register file (read on 0) (write on 1)
 	  
-	   
-	   
-	   
-	   
+
 	   constOp : in std_logic_vector(3 downto 0); 
 	   clk : in std_logic;
 	   reset : in std_logic;
@@ -93,7 +90,7 @@ architecture ALUArch of ALU_DATAPATH is
 	    port(
 
 			valA : in std_logic_vector((operandLength - 1) downto 0);
-			const : in std_logic_vector((operandLength - 1) downto 0);
+			valB : in std_logic_vector((operandLength - 1) downto 0);
 			opcode : in std_logic_vector((opcodeLength - 1) downto 0);
 
 			outVal : out std_logic_vector((operandLength - 1) downto 0);
@@ -105,24 +102,110 @@ architecture ALUArch of ALU_DATAPATH is
 
 
 
-	signal regToMux_OpA : std_logic_vector(3 downto 0); --operand A from regfile
+	signal regToFU_OpA : std_logic_vector(3 downto 0); --operand A from regfile
 	signal regToMux_OpB : std_logic_vector(3 downto 0); --operand B from regfile
-	signal constToMux : std_logic_vector(3 downto 0); --redundant but added anyways
+	--signal constToMux : std_logic_vector(3 downto 0); --redundant but added anyways
 	signal operandMuxToFU : std_logic_vector(3 downto 0); --carries Op B or const to FU
-	signal dataFromMemory : std_logic_vector(3 downto 0); --data to be loaded from RAM
-	signal constOrBSelector : std_logic; --wire to const or operand B mux
-	signal FUResultOrRAM : std_logic; --wire to selector data from FU or RAM
+	--signal dataFromMemory : std_logic_vector(3 downto 0); --data to be loaded from RAM
 	
-	signal PZN_Out : std_logic_vector(2 downto 0); --pos, zero, neg from FU operation
+	--signal PZN_Out : std_logic_vector(2 downto 0); --pos, zero, neg from FU operation
 	signal FUResultToMux : std_logic_vector(3 downto 0); --from FU to result or data in selection
 	signal muxToRegisterFileDest : std_logic_vector(3 downto 0); --carries either result or data in to the register file data in port
 	
 	
-
+	--CONTROL WORD SIGNALS
+	signal A_Addr : std_logic_vector(2 downto 0); --bits [15:13]
+	signal B_Addr : std_logic_vector(2 downto 0); --bits [11:9]
+	signal constOrBSelector : std_logic; --wire to const or operand B mux, bit 12
+	signal FUResultOrRAM : std_logic; --wire to selector data from FU or RAM, bit 8
+    signal opcodeSelect : std_logic_vector(3 downto 0); --bits [7:4]
+    signal regfileDestinationAddr : std_logic_vector(2 downto 0); --bits [3:1]
+    signal readOrWrite : std_logic; --read regfile or write to regfile, bit 0
+    
 begin
 
+    --CONTROL WORD MAPPING
+    A_Addr                  <= ControlWord(15 downto 13);
+    constOrBSelector        <= ControlWord(12);
+    B_Addr                  <= ControlWord(11 downto 9);
+    FUResultOrRam           <= ControlWord(8);
+    opcodeSelect            <= ControlWord(7 downto 4);
+    regFileDestinationAddr  <= ControlWord(3 downto 1);
+    readOrWrite             <= ControlWord(0);
+    
+    dataOut <= operandMuxToFU; --from ALU to ram
+    addrOut <= regToFU_OpA; --from ALU to ram
+    
+    ALU_REGFILE: Register_File port map(
+    
+            Destination_Data => muxToRegisterFileDest,
+            Destination_Address => regFileDestinationAddr,
+            
+            register_a_Address => A_Addr,
+            register_B_Address => B_Addr,
+            
+            clk_regfile => clk,
+            reset_regfile => reset,
+            read_write => readOrWrite,
+            
+            Out_Data_A => regToFU_OpA,
+            Out_data_B => regToMux_OpB
+            
+    
+        );
+        
+        
+        --used to seleect if a constant or value from register addr B should be an operand
+        OPERAND_MUX: mux generic map( datalength => 4,
+                                      selectorlength => 1)
+                        
+                        port map(
+                        
+                            data_in(1) => regToMux_OpB,
+                            data_in(0) => constOp,
+                            
+                            selector => (0 => constOrBSelector,
+                                         others => '0'),
+                                         
+                            data_out => operandMuxToFU
+                            
+                        
+                        
+                        );
 
+        
+        
+        FUorRAM_MUX: mux generic map(datalength => 4,
+                                     selectorlength =>1)
+                         port map(
+                         
+                            data_in(1) => FUResultToMux,
+                            data_in(0) => dataFromRam,
+                            
+                            selector => (0 => FUResultOrRAM,
+                                         others => '0'),
+                                         
+                            data_out => muxToRegisterFileDest
+                         
+                         
+                         );
+        
+        
+        ALU_FUNCTION_UNIT: Functional_Unit generic map(operandLength => 4,
+                                                    opcodeLength => 4,
+                                                    numFunctions => 16)
+                                      port map(
+                                      
+                                        valA => regToFU_OpA,
+                                        valB => operandMuxToFU,
+                                        
+                                        opcode => opcodeSelect,
+                                        
+                                        outVal => FUResultToMux,
+                                        poszeroneg => PZN
+                                      
 
-
+                                      );
+        
 
 end ALUArch;
