68bdb83133eb0e4daa42394e3cefd0c40f6e12ff
mechanicallamb <60459171+mechanicallamb@users.noreply.github.com>
2022-01-15 02:38:23 -0500

    Final update to ALU.vhd
    Updated ALU and ALU testbench and verified operation of the ALU and all components. ALU now confirmed to produce correct results of bitlength 4, and assumed any arbitrary bitlength, which will be test later.

--- ALU.vhd
+++ ALU.vhd
@@ -23,7 +23,7 @@ entity ALU_DATAPATH is
 	   -- [15:13] Reg File Address A for operand A
 	   -- [12] Operand B as constant (case 1) or operand B from reg file address B (case 0)
 	   -- [11:9] Address of regfile register B (regardless of assertion of bit 12)
-	   -- [8] Load data from memory flag (case 0: no store value from function unit) (case 1: load from ram into regfile address DA)
+	   -- [8] Load data from memory flag (case 0: no, store value from function unit) (case 1: load from ram into regfile address DA)
 	   -- [7:4] OPCODE for function unit
 	   -- [3:1] DA, the register to be written to on a write
 	   -- [0] read or write to register file (read on 0) (write on 1)
