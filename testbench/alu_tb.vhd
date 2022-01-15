350550c45efbe53049943fcadfb14de712a4eed6
mechanicallamb <60459171+mechanicallamb@users.noreply.github.com>
2022-01-15 02:39:10 -0500

    Final ALU testbench update
    This testbench has been used to verify the operation of the ALU

--- testbench/alu_tb.vhd
+++ testbench/alu_tb.vhd
@@ -143,9 +143,232 @@ begin
         
         wait for 10 ns;
         
+        --store 1011 in register 1
+        constop_tb <= "1011";
+        controlWord_tb <= "0001000000010011";
+        clk_tb <= '1';
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        --store 1010 in register 2
+        constop_tb <= "1010";
+        controlWord_tb <= "0001000000010101";
+        clk_tb <= '1';
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        --store 1001 in register 3
+        constop_tb <= "1001";
+        controlWord_tb <= "0001000000010111";
+        clk_tb <= '1';
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        --store 1000 in register 4
+        constop_tb <= "1000";
+        controlWord_tb <= "0001000000011001";
+        clk_tb <= '1';
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        --store 0111 in register 5
+        constop_tb <= "0111";
+        controlWord_tb <= "0001000000011011";
+        clk_tb <= '1';
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        --store 0110 in register 6
+        constop_tb <= "0110";
+        controlWord_tb <= "0001000000011101";
+        clk_tb <= '1';
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        --store 0101 in register 7
+        constop_tb <= "0101";
+        controlWord_tb <= "0001000000011111";
+        clk_tb <= '1';
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        
+        --format
+        --regfile values registers [0:7]
+        --[c, b, a, 9, 8, 7, 6, 5]
+        
+        
+        --pass register 4 and store into register 6
+        --result is reg6 = 8
+        --[c b a 9 8 7 8 5]
+        controlWord_tb <= "1000000000001101";
         clk_tb <= '1';
         
         wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+         
+        --add registers 2 and 5, store in 4
+        --result = reg4 = 1
+        --[c b a 9 1 7 8 5]
+        
+        controlWord_tb <= "0100101000101001";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        --subtract register 7 from register 0, store reg 0
+        --result = reg0 = 7
+        --[7 b a 9 1 7 8 5]
+        
+        controlWord_tb <= "0000111000110001";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        
+        --right shift register 1 and store in register 4
+        --result = reg4 = 5
+        --[7 b a 9 5 7 8 5]
+        
+        controlWord_tb <= "0010000001001001";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        
+        
+        --invert register 1 and store in register 1
+        --result = reg1 = 0100
+        --[7 4 a 9 5 7 8 5]
+        
+        controlWord_tb <= "0010000001010011";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        -- and reg 4 and register 3, store reg 7
+        -- result = reg7 = 1
+        --[7 4 a 9 5 7 8 1]
+        
+        controlWord_tb <= "1000011001101111";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        
+        --total
+        --[7 4 a 9 5 7 8 1]
+        
+        stop;
+        
+        
+        
+        
+        --if B = 0, pass A, else pass 0
+        
+        --B != 0, pass 0
+        --compare reg 4 with 0, store 0 into reg 6
+        --result = [7 4 a 9 5 7 0 1]
+        
+        controlWord_tb <= "0110100001111101";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        
+        --B = 0, pass and store a into reg 2
+        --compare reg 6 with 0, store reg 3 in reg2
+        --result = [7 4 9 9 5 7 0 1]
+        
+        controlWord_tb <= "0110110001110101";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        --if A = B, pass 1, else pass 0
+        --compare reg 0 and 5, store 1 into reg 3
+        --result [7 4 9 0 5 7 0 1]
+        
+        controlWord_tb <= "0000101010000111";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        
+        -- A != B, store 0 in reg 0
+        --compare register 1 and register 0
+        --store 0 in reg 0
+        --result [1 4 9 0 5 7 0 1]
+        
+        controlWord_tb <= "0010000010000001";
+        clk_tb <= '1';
+        
+        wait for 10 ns;
+        
+        clk_tb <= '0';
+        
+        wait for 10 ns;
+        
+        
+        stop;
+        
+        
         
         stop;
         
